ac79b9f4bcf403d5b539111c93f49b4feabd01b6
Adam Langsner <adamlangsner@gmail.com>
2021-02-11 19:49:55 -0500

    mvoe initializer into class definition

--- lib/heroicon/engine.rb
+++ lib/heroicon/engine.rb
@@ -3,10 +3,11 @@
 module Heroicon
   class Engine < ::Rails::Engine
     isolate_namespace Heroicon
+
+    initializer "heroicon.assets.precompile" do |app|
+      app.config.assets.precompile += %w(**/*.svg)
+    end
   end
 end
 
 
-initializer "heroicon.assets.precompile" do |app|
-  app.config.assets.precompile += %w(**/*.svg)
-end
